ab5dd8459ce9e547e27b810471812d285ff915db
semperprimum <bkeem0266+github@gmail.com>
2023-01-30 16:04:17 +0600

    seventh commit

--- calc.php
+++ calc.php
@@ -1,3 +1,34 @@
+<?php 
+$operator = $_POST['operator'];
+$num1 = $_POST['num1'];
+$num2 = $_POST['num2'];
+$result = null;
+$warning = '';
+
+switch($operator) {
+  case '+';
+  $result = intval($num1) + intval($num2);
+  break;
+
+  case '-';
+  $result = intval($num1) - intval($num2);
+  break;
+
+  case '*';
+  $result = intval($num1) * intval($num2);
+  break;
+
+  case '/';
+  if ($num2 == 0) {
+    $warning = 'На ноль делить нельзя!';
+  } elseif ($num2 != 0) {
+    $warning = '';
+    $result = intval($num1) / intval($num2);
+  }
+  break;
+}
+?>
+
 <!DOCTYPE html>
 <html>
 
@@ -9,7 +40,7 @@
 
 <body>
     <!-- Область основного контента -->
-    <form action=''>
+    <form action='' method="post">
       <label>Число 1:</label>
       <br />
       <input name='num1' type='text' />
@@ -23,7 +54,9 @@
       <input name='num2' type='text' />
       <br />
       <br />
-      <input type='submit' value='Считать'>
+      <input type='submit' value='Считать'> <br>
+      <h2> <?= $num1 . ' ' . $operator . ' ' . $num2 . ' = ' . $result; ?> </h2>
+      <h1 style="color: red;"> <?= $warning ?></h1>
     </form>
     <!-- Область основного контента -->
 </body>
